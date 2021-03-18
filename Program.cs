39b54d49f6795b7f10f749fc6df372021d634d74
Maru Amallo <amamarul@gmail.com>
2021-03-17 22:53:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A827141.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# A827141.Actividad01
+
+# Instructions
+
+## Using Visual Studio Code  
+
+Run the following command  
+```
+dotnet run
+```
